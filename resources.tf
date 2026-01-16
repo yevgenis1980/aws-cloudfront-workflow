cf7d2b9776f41ba9152198921fcacecbde2ecfb4
Yevgeni Shapiro <23049337+yevgenis1980@users.noreply.github.com>
2026-01-17 01:18:54 +0200

    update

--- resources.tf
+++ resources.tf
@@ -1,20 +1,24 @@
 
+# -----------------------------
+#     Global Modules 
+# -----------------------------
+
 module "vpc" {
 source = "./modules/vpc"
 cidr_block = "10.0.0.0/16"
 project = var.project_name
 }
 
-
 module "s3" {
 source = "./modules/s3"
 bucket_name = var.project_name
+depends_on = [module.vpc]
 }
-
 
 module "cloudfront" {
 source = "./modules/cloudfront"
 bucket_domain_name = module.s3.bucket_domain_name
 bucket_arn = module.s3.bucket_arn
 project = var.project_name
+depends_on = [module.s3]
 }
