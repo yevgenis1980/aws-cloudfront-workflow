d95af520e6f9f21a9719283e1096ae80119a05b9
Yevgeni Shapiro <23049337+yevgenis1980@users.noreply.github.com>
2026-01-16 22:24:20 +0200

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,33 +1,8 @@
 
-output "vpc_id" {
-  description = "ID of the main VPC"
-  value       = aws_vpc.main.id
+variable "aws_region" {
+default = "us-west-2"
 }
 
-output "public_subnet_ids" {
-  description = "IDs of the public subnets"
-  value       = [for s in aws_subnet.public : s.id]
-}
-
-output "private_subnet_ids" {
-  description = "IDs of the private subnets"
-  value       = [for s in aws_subnet.private : s.id]
-}
-
-output "internet_gateway_id" {
-  description = "ID of the Internet Gateway"
-  value       = aws_internet_gateway.igw.id
-}
-
-output "nat_gateway_id" {
-  description = "ID of the NAT Gateway"
-  value       = aws_nat_gateway.nat.id
-}
-
-output "s3_bucket_name" {
-  value = aws_s3_bucket.static_site.bucket
-}
-
-output "cloudfront_url" {
-  value = aws_cloudfront_distribution.cdn.domain_name
+variable "project_name" {
+default = "static-site"
 }
