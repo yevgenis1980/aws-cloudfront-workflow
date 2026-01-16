90609a57da74f9ede2edbae7d42caa6f3eda2454
Yevgeni Shapiro <23049337+yevgenis1980@users.noreply.github.com>
2026-01-16 18:40:14 +0200

    update

--- resources.tf
+++ resources.tf
@@ -1,58 +1,111 @@
 
-resource "aws_lb" "app_alb" {
-  name               = "app-alb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.alb_sg.id]
-  subnets            = aws_subnet.public[*].id
-
-  enable_deletion_protection = false
+resource "aws_s3_bucket" "static_site" {
+  bucket = "my-static-site-${random_id.bucket_id.hex}"
 
   tags = {
-    Name = "app-alb"
+    Name = "static-site"
   }
-   depends_on = [aws_autoscaling_group.asg]
+}
+
+resource "random_id" "bucket_id" {
+  byte_length = 4
+}
+
+resource "aws_s3_bucket_website_configuration" "static" {
+  bucket = aws_s3_bucket.static_site.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "block" {
+  bucket = aws_s3_bucket.static_site.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 
-resource "aws_lb_target_group" "app_tg" {
-  name     = "app-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
 
-  health_check {
-    path                = "/"
-    protocol            = "HTTP"
-    matcher             = "200"
-    interval            = 30
-    healthy_threshold   = 3
-    unhealthy_threshold = 3
-    timeout             = 5
+
+resource "aws_cloudfront_origin_access_control" "oac" {
+  name                              = "static-site-oac"
+  description                       = "OAC for static site"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_distribution" "cdn" {
+  enabled = true
+  default_root_object = "index.html"
+
+  origin {
+    domain_name              = aws_s3_bucket.static_site.bucket_regional_domain_name
+    origin_id                = "s3-static-site"
+    origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "s3-static-site"
+
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
   }
 
   tags = {
-    Name = "app-tg"
+    Name = "static-site-cdn"
   }
-  depends_on = [aws_autoscaling_group.asg]
 }
 
 
-resource "aws_lb_listener" "http_listener" {
-  load_balancer_arn = aws_lb.app_alb.arn
-  port              = "80"
-  protocol          = "HTTP"
+data "aws_iam_policy_document" "s3_policy" {
+  statement {
+    actions = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.static_site.arn}/*"]
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.app_tg.arn
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = [aws_cloudfront_distribution.cdn.arn]
+    }
   }
-  depends_on = [aws_autoscaling_group.asg]
 }
 
-
-resource "aws_autoscaling_attachment" "asg_alb_attachment" {
-  autoscaling_group_name = aws_autoscaling_group.asg.name
-  lb_target_group_arn    = aws_lb_target_group.app_tg.arn
+resource "aws_s3_bucket_policy" "allow_cloudfront" {
+  bucket = aws_s3_bucket.static_site.id
+  policy = data.aws_iam_policy_document.s3_policy.json
 }
+
 
