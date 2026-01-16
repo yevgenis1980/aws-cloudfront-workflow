42f5a605f79c514c939c8cafbacde41824841446
Yevgeni Shapiro <23049337+yevgenis1980@users.noreply.github.com>
2026-01-17 01:28:31 +0200

    update

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,3 +1,24 @@
 output "vpc_id" {
-value = aws_vpc.this.id
+  description = "ID of the main VPC"
+  value       = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  description = "IDs of the public subnets"
+  value       = [for s in aws_subnet.public : s.id]
+}
+
+output "private_subnet_ids" {
+  description = "IDs of the private subnets"
+  value       = [for s in aws_subnet.private : s.id]
+}
+
+output "internet_gateway_id" {
+  description = "ID of the Internet Gateway"
+  value       = aws_internet_gateway.igw.id
+}
+
+output "nat_gateway_id" {
+  description = "ID of the NAT Gateway"
+  value       = aws_nat_gateway.nat.id
 }
