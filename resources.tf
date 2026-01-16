4bb4745a4d5814151ffef1cd7abf815019ea3e36
Yevgeni Shapiro <23049337+yevgenis1980@users.noreply.github.com>
2026-01-16 22:25:10 +0200

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -1,122 +1,20 @@
 
-# -----------------------------
-#           S3_Bucket
-# -----------------------------
-resource "aws_s3_bucket" "static_site" {
-  bucket = "my-static-site-${random_id.bucket_id.hex}"
-
-  tags = {
-    Name = "static-site"
-  }
-}
-
-resource "random_id" "bucket_id" {
-  byte_length = 4
-}
-
-resource "aws_s3_bucket_website_configuration" "static" {
-  bucket = aws_s3_bucket.static_site.id
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "error.html"
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "block" {
-  bucket = aws_s3_bucket.static_site.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
-# -----------------------------
-#          CloudFront 
-# -----------------------------
-resource "aws_cloudfront_origin_access_control" "oac" {
-  name                              = "static-site-oac"
-  description                       = "OAC for static site"
-  origin_access_control_origin_type = "s3"
-  signing_behavior                  = "always"
-  signing_protocol                  = "sigv4"
-}
-
-resource "aws_cloudfront_distribution" "cdn" {
-  enabled = true
-  default_root_object = "index.html"
-
-  origin {
-    domain_name              = aws_s3_bucket.static_site.bucket_regional_domain_name
-    origin_id                = "s3-static-site"
-    origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
-  }
-
-  default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "s3-static-site"
-
-    viewer_protocol_policy = "redirect-to-https"
-
-    forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
-    }
-  }
-
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
-
-  viewer_certificate {
-    cloudfront_default_certificate = true
-  }
-
-  tags = {
-    Name = "static-site-cdn"
-  }
+module "vpc" {
+source = "./modules/vpc"
+cidr_block = "10.0.0.0/16"
+project = var.project_name
 }
 
 
-data "aws_iam_policy_document" "s3_policy" {
-  statement {
-    actions = ["s3:GetObject"]
-    resources = ["${aws_s3_bucket.static_site.arn}/*"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["cloudfront.amazonaws.com"]
-    }
-
-    condition {
-      test     = "StringEquals"
-      variable = "AWS:SourceArn"
-      values   = [aws_cloudfront_distribution.cdn.arn]
-    }
-  }
-}
-
-resource "aws_s3_bucket_policy" "allow_cloudfront" {
-  bucket = aws_s3_bucket.static_site.id
-  policy = data.aws_iam_policy_document.s3_policy.json
-}
-
-resource "aws_s3_object" "index" {
-  bucket = aws_s3_bucket.static_site.id
-  key    = "index.html"
-  source = "site/index.html"
-  content_type = "text/html"
+module "s3" {
+source = "./modules/s3"
+bucket_name = var.project_name
 }
 
 
-
+module "cloudfront" {
+source = "./modules/cloudfront"
+bucket_domain_name = module.s3.bucket_domain_name
+bucket_arn = module.s3.bucket_arn
+project = var.project_name
+}
