26478358f9dd62c03e17a994d3b63e425a22e2e0
Yevgeni Shapiro <23049337+yevgenis1980@users.noreply.github.com>
2026-01-17 01:14:58 +0200

    update

--- modules/cloudfront/main.tf
+++ modules/cloudfront/main.tf
@@ -1,74 +1,65 @@
 
-resource "aws_cloudfront_origin_access_control" "this" {
-name = "${var.project}-oac"
-origin_access_control_origin_type = "s3"
-signing_behavior = "always"
-signing_protocol = "sigv4"
-}
 
+resource "aws_cloudfront_origin_access_control" "this" {
+  name                              = "${var.project}-oac"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
 
 resource "aws_cloudfront_distribution" "this" {
-enabled = true
-default_root_object = "index.html"
+  enabled             = true
+  default_root_object = "index.html"
 
+  origin {
+    domain_name              = var.bucket_domain_name
+    origin_id               = "s3-origin"
+    origin_access_control_id = aws_cloudfront_origin_access_control.this.id
+  }
 
-origin {
-domain_name = var.bucket_domain_name
-origin_id = "s3-origin"
-origin_access_control_id = aws_cloudfront_origin_access_control.this.id
+  default_cache_behavior {
+    target_origin_id       = "s3-origin"
+    viewer_protocol_policy = "redirect-to-https"
+
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods  = ["GET", "HEAD"]
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
 }
 
-
-default_cache_behavior {
-target_origin_id = "s3-origin"
-viewer_protocol_policy = "redirect-to-https"
-
-
-allowed_methods = ["GET", "HEAD"]
-cached_methods = ["GET", "HEAD"]
-
-
-forwarded_values {
-query_string = false
-
-
-cookies {
-forward = "none"
-}
-}
-}
-
-
-restrictions {
-geo_restriction {
-restriction_type = "none"
-}
-}
-
-
-viewer_certificate {
-cloudfront_default_certificate = true
-}
-}
-
-
-# Allow CloudFront to read S3
 
 
 data "aws_iam_policy_document" "this" {
-statement {
-actions = ["s3:GetObject"]
-resources = ["${var.bucket_arn}/*"]
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${var.bucket_arn}/*"]
 
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
 
-principals {
-type = "Service"
-identifiers = ["cloudfront.amazonaws.com"]
-}
-
-
-condition {
-test = "StringEquals"
-variable = "AWS:SourceArn"
-values = [aws_cloudfront_distribution.this.arn]
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = [aws_cloudfront_distribution.this.arn]
+    }
+  }
 }
