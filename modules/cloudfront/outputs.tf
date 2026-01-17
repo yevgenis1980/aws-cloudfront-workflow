d8dfcf0678d35bf4586af7ba60bb2f97887c5bf1
Yevgeni Shapiro <23049337+yevgenis1980@users.noreply.github.com>
2026-01-17 23:50:20 +0200

    update

--- modules/cloudfront/outputs.tf
+++ modules/cloudfront/outputs.tf
@@ -4,4 +4,12 @@ output "domain_name" {
   value = aws_cloudfront_distribution.this.domain_name
 }
 
+output "cloudfront_domain_name" {
+  description = "The domain name of the CloudFront distribution"
+  value       = aws_cloudfront_distribution.this.domain_name
+}
 
+output "cloudfront_distribution_id" {
+  description = "The ID of the CloudFront distribution"
+  value       = aws_cloudfront_distribution.this.id
+}
